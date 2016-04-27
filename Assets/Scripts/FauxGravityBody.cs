f9a9d60544cab70476dc5b50eb23aa617df7ed25
Alec Arme <alec.arme@heliosinteractive.com>
2016-04-27 16:47:28 -0700

    initial commit

---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class FauxGravityBody : MonoBehaviour {
+
+	public FauxGravityAttractor attractor;
+	private Transform myTransform;
+    private Rigidbody myRigidBody;
+
+	void Start () {
+        myRigidBody = GetComponent<Rigidbody>();
+		GetComponent<Rigidbody>().useGravity = false;
+		GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+
+		myTransform = transform;
+	}
+
+	void FixedUpdate () {
+
+        myRigidBody.AddForce(transform.forward * 5);
+
+		if (attractor){
+			attractor.Attract(myTransform);
+		}
+	}
+	
+}
